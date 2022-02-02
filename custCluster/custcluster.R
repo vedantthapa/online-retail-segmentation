31e4cd57975419f0121f01b686020b952513d596
vedantthapa <thapavedant@gmail.com>
2022-02-02 17:45:18 -0400

    Organize directories

---
+++ custCluster/custcluster.R
@@ -0,0 +1,2 @@
+data = read.csv2("Downloads/online-retail-clustering/custCluster/custcluster.csv", col.names = c("customerID", "prod_purchased", "basket", "revenue", "num_visits", "avg_spend"))
+head(data)
---
+++ prodCluster/prodcluster.R
@@ -0,0 +1,2 @@
+data = read.csv2("Downloads/online-retail-clustering/prodCluster/prodcluster.csv", col.names = )
+head(data)
