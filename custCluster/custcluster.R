8b47e0b460c1c846d403cf3cf5a1058d4ce98f60
vedantthapa <thapavedant@gmail.com>
2022-02-03 20:38:28 -0400

    Updates R code for standardization of variables

--- custCluster/custcluster.R
+++ custCluster/custcluster.R
@@ -2,9 +2,9 @@ library(ggplot2)
 library(GGally)
 library(dplyr)
 
-set.seed(42)
-
-data = read.csv2("Downloads/online-retail-clustering/custCluster/custcluster.csv", col.names = c("customerID", "prod_purchased", "basket", "revenue", "num_visits", "avg_spend"))
+data = read.csv2("~/Downloads/online-retail-segmentation/custCluster/custcluster.csv", 
+                 col.names = c("customerID", "prod_purchased", "basket", "revenue", "num_visits", "avg_spend"),
+                 stringsAsFactors = FALSE)
 head(data)
 
 # checking for missing values
@@ -12,40 +12,42 @@ sum(is.na(data))
 
 sapply(data, class)
 cols.num = c("revenue", "avg_spend")
-data[cols.num] <- sapply(data[cols.num],as.numeric)
+data[cols.num] = sapply(data[cols.num],as.numeric)
 
 sapply(data, class)
+head(data)
 
-ggpairs(data[which(names(data) != "customerID")], upper = list(continuous = ggally_points), lower = list(continuous = "points"), title = "Products before outlier removal")
+ggpairs(data[which(names(data) != "customerID")], 
+        upper = list(continuous = ggally_points), 
+        lower = list(continuous = "points"), 
+        title = "Products before outlier removal")
+
+cor(data, method = "pearson")
+
 summary(data)
 
-sum(data[data$revenue > 0, ]$revenue)
+data.scale = scale(data[-1])
 
-data.scale = scale(data[-1], scale=FALSE)
-
-data.scale
-
-source("~/Downloads/online-retail-clustering/custCluster/multikmeans.r")
-err = multiKmeans(data.scale, 1, 10, 1000)
+source("~/Downloads/online-retail-segmentation/multikmeans.r")
+err = multiKmeans(data.scale, 1, 15, 1000)
 plot(err)
 
-k4 = kmeans(data, centers = 4, iter.max = 1000)
-k4$cluster
-
+set.seed(42)
+k4 = kmeans(data.scale, centers = 4, iter.max = 1000)
 
 data %>%
  mutate(Cluster = k4$cluster) %>%
  group_by(Cluster) %>%
- summarise_all("mean")
+ summarise_all("median")
 
 # 1 - Frequent
 # 2 - Occasional
 # 3 - Sale
 # 4 - SME
 
-k4$cluster
-data
-data$cluster = k4$cluster
-data[data$cluster == 1, ]
+# data$cluster = k4$cluster
+# data[data$cluster == 1, ]
 
-k4$size
+
+
+
